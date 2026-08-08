02d2b38cd2c1ff042c37d9bb91960389a94f9dee
Eloitor <Eloitor@users.noreply.github.com>
2026-08-08 15:04:58 +0000

    deploy: bf30fdf6e4843b4eb9b2ced432ec4f9e68ac58c3

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8880774b.js',
+                path_to_root + 'searchindex-c400e9fc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
